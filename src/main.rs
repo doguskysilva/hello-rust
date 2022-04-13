d7c2082e55d20581c9e78c1aeb567907f0089f32
Douglas Silva <dogusky.silva@outlook.com>
2022-04-13 12:40:17 -0300

    create the guess game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{stdin};
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        stdin().read_line(&mut guess).expect("Failed to readline");
+        
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number");
+                continue
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Tooo Small!"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win!");
+                break
+            }
+        }
+    }
 }
